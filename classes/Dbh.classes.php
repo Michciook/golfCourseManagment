1c4a748e85807e4414afbf3c6dabfe26249bc0ed
michciook <michtabiznes@gmail.com>
2024-01-29 19:30:11 +0100

    added simple login system

--- classes/Dbh.classes.php
+++ classes/Dbh.classes.php
@@ -1,16 +1,10 @@
 <?php
+include "../db_conf.php";
 
 class Dbh {
-    private $host = '54.38.50.59';
-    private $username = 'www13358_golf';
-    private $password = 'EhTkvrjkxsrP3xHl86Eh';
-    private $dbname = 'www13358_golf';
-
     protected function connection() {
         try {
-            $pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname;port=3306", 
-            $this->username, 
-            $this->password);
+            $pdo = new PDO("mysql:host=HOST;dbname=DBNAME;port=3306", DBUSER, DBPASSWD);
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
